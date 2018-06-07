237ab1aa848a24fc7f77611d0c740386044fe84b
Liuzz <754870471@qq.com>
2018-06-07 11:39:38 +0800

    基本模块完成

---
+++ src/components/subjective-evaluation-design/SED-pgzEntryFlameout.vue
@@ -0,0 +1,232 @@
+<template>
+	<div class="SED_pgzEntryFlameout">
+		<div class="title">
+			<span class="return" @click="toReturn"><i class="el-icon-arrow-left"></i>返回	</span>
+			<span>测试评估</span>
+		</div>
+		<div class="chooseSite" v-show="chooseSiteShow">
+			<img src="../../../static/bgCar2.png" style="width: 320px;">
+			<span v-for="(item,index) in xihuoAr" style="position: absolute;" @click="chooseTemperature(item,index)">{{item.name}}</span>
+		</div>
+		<div class="temList">
+			<span v-for="item in xihuoAr" style="display: inline-block;width: 45%;">
+				<span style="display: inline-block;width: 90px;text-align: right;">{{item.name}}</span>：{{item.score}}
+			</span>
+		</div>
+		<div class="sliderWrapper" v-show="sliderShow">
+			<div class="chooseLevel">
+				<el-slider
+					:format-tooltip="formatTooltip"
+				  v-model.number="userLevel"
+				  vertical
+				  :step="0.1"
+				  :min="0"
+				  :max="10"
+				  height="300px">
+				</el-slider>
+			</div>
+			<div style="display: inline-block;height: 300px;width: 40px;font-size:14px;position: relative;top: -15px;">
+				<p style="display: block;height: 60px;border-top: 1px solid #409EFF;">烫伤</p>
+				<p style="display: block;height: 60px;border-top: 1px solid #409EFF;">烫</p>
+				<p style="display: block;height: 60px;border-top: 1px solid #409EFF;">温热</p>
+				<p style="display: block;height: 60px;border-top: 1px solid #409EFF;">温</p>
+				<p style="display: block;height: 60px;border-top: 1px solid #409EFF;">舒适</p>
+			</div>
+			<div class="button">
+				<el-button type="success" style="width: 120px;" @click="chooseTemperatureOK">确定</el-button>
+			</div>
+		</div>
+		<div class="button">
+			<el-button type="primary" style="width: 120px;" @click="submit">提交</el-button>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				siteAr: ['Driver', '1R', '2L', '2R', '3L', '3R'],
+				chooseSiteShow: true,
+				sliderShow: false,
+				nowCurrentIndex:-1,
+				userLevel: 0,
+				xihuoAr: [
+					{
+						name:'支撑杆护套',
+						score:0
+					},
+					{
+						name:'翼子板',
+						score:0
+					},
+					{
+						name:'后备箱地毯',
+						score:0
+					},
+					{
+						name:'制动液壶盖',
+						score:0
+					},
+					{
+						name:'冷却液壶盖',
+						score:0
+					},
+					{
+						name:'机油标尺',
+						score:0
+					},
+					{
+						name:'保险丝盒',
+						score:0
+					},
+					{
+						name:'机油加注口',
+						score:0
+					},
+					{
+						name:'前舱盖锁头',
+						score:0
+					},
+					{
+						name:'引擎盖',
+						score:0
+					}
+				]
+			}
+		},
+		created() {
+			this.chooseSiteShow = true
+		},
+		methods: {
+			submit(){
+				this.$router.push('/SED_table')
+			},
+			chooseTemperatureOK(){
+				this.sliderShow=false
+				this.$set(this.xihuoAr[this.nowCurrentIndex],'score',this.userLevel)
+			},
+			chooseTemperature(item,index){
+				this.sliderShow=true
+				this.nowCurrentIndex = index
+			},
+			toReturn() {
+				this.$router.go(-1)
+			},
+			chooseOk() {
+				this.$router.push('/SED_table')
+			},
+			formatTooltip(val) {
+				if(val > 0 && val <= 2) {
+					$('.el-slider__bar').css("background", "#0C6FF9")
+				} else if(val > 2 && val <= 4) {
+					$('.el-slider__bar').css("background", "#02BC05")
+				} else if(val > 4 && val <= 6) {
+					$('.el-slider__bar').css("background", "#F9B60C")
+				} else if(val > 6 && val <= 7) {
+					$('.el-slider__bar').css("background", "#F99E0C")
+				} else if(val > 8) {
+					$('.el-slider__bar').css("background", "#F90C0C")
+				}
+			},
+		}
+	}
+</script>
+
+<style lang="scss">
+.el-slider__bar {
+		background-color: red;
+	}
+	
+	.SED_pgzEntryFlameout {
+		>.title {
+			position: relative;
+			height: 40px;
+			line-height: 40px;
+			text-align: center;
+			border-bottom: 1px solid rgb(231,231,231);
+			>.return {
+				position: absolute;
+				left: 0;
+				top: 0;
+				padding: 0px 5px;
+			}
+		}
+		.sliderWrapper{
+			position: fixed;
+			top: 0;
+			left: 0;
+			right: 0;
+			bottom: 0;
+			background: #ffffff;
+			.chooseLevel {
+				position: relative;
+				top: 20px;
+				left: 10px;
+				display: inline-block;
+			}
+		}
+		.chooseSite {
+			display: block;
+			margin-top: 40px;
+			position: relative;
+			span {
+				font-size: 14px;
+				background: #ffffff;
+				padding: 2px 5px;
+				border: 1px solid #409EFF;
+				&:nth-child(2) {
+					top: 10px;
+					left: 0;
+				}
+				&:nth-child(3) {
+					top: 170px;
+					left: 0;
+				}
+				&:nth-child(4) {
+					top: 5px;
+					right: 30px;
+				}
+				&:nth-child(5) {
+					top: 60px;
+					right: 120px;
+				}
+				&:nth-child(6) {
+					top: 60px;
+					right: 5px;
+				}
+				&:nth-child(7) {
+					top: 100px;
+					right: 110px;
+				}
+				&:nth-child(8) {
+					top: 100px;
+					right: 0px;
+				}
+				&:nth-child(9) {
+					top: 130px;
+					right: 120px;
+				}
+				&:nth-child(10) {
+					top: 190px;
+					right: 70px;
+				}
+				&:nth-child(11) {
+					bottom: 10px;
+					left: 70px;
+				}
+			}
+		}
+	
+	
+	
+	
+	.button {
+		position: fixed;
+		bottom: 30px;
+		left: 0;
+		right: 0;
+		text-align: center;
+	}
+}
+</style>
